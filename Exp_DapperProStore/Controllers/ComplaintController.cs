8de14a313d4aa161808581ad5be1cfc880d3bfc7
euroland3 <euroland3@EUROLAND3-PC>
2017-12-05 16:27:34 +0700

    Example get-return-value-from-stored-procedure-using-dapper-orm

---
+++ Exp_DapperProStore/Controllers/ComplaintController.cs
@@ -0,0 +1,33 @@
+﻿using Exp_DapperProStore.Models;
+using Exp_DapperProStore.Reponsitory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exp_DapperProStore.Controllers
+{
+    public class ComplaintController : Controller
+    {
+        // GET: Complaint
+        public ActionResult AddComplaint()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddComplaint(ComplaintModel ObjComp)
+        {
+            try
+            {
+                ComplaintRepo Obj = new ComplaintRepo();
+                ViewBag.ComplaintId = "Successfully, Your Id: " + Obj.AddComplaint(ObjComp);
+            }
+            catch (Exception)
+            {
+                ViewBag.ComplaintId = "error, please check details";
+            }
+            return View();
+        }
+    }
+}
---
+++ Exp_DapperProStore/Reponsitory/ComplaintRepo.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Exp_DapperProStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Exp_DapperProStore.Reponsitory
+{
+    public class ComplaintRepo
+    {
+        SqlConnection con;
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["SqlConn"].ToString(); 
+            con = new  SqlConnection(constr);
+        }
+        public string AddComplaint(ComplaintModel Obj)
+        {
+            DynamicParameters ObjParm = new DynamicParameters();
+            ObjParm.Add("@ComplaintType", Obj.ComplaintType);
+            ObjParm.Add("@ComplaintDesc", Obj.ComplaintDesc);
+            ObjParm.Add("@ComplaintId", dbType: System.Data.DbType.String, direction: System.Data.ParameterDirection.Output, size: 951652);
+            con.Open();
+            con.Execute("AddComplaint", ObjParm, commandType: CommandType.StoredProcedure);
+            var ComplaintId = ObjParm.Get<string>("@ComplaintId");
+            con.Close();
+            return ComplaintId;
+        }
+    }
+}
